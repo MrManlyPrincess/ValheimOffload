092ae125f32742d15b16ce7d6e6a778c47cfe888
Gavin <Neyen13@gmail.com>
2022-06-09 12:22:34 -0500

    Update publish script so readme changes propagate between GitHub and Thunderstore

--- publish.ps1
+++ publish.ps1
@@ -71,6 +71,7 @@ if($Target.Equals("Release")) {
 
     Write-Host "$PackagePath\$TargetAssembly"
     Copy-Item -Path "$TargetPath\$TargetAssembly" -Destination "$PackagePath\plugins\$TargetAssembly" -Force
+    Copy-Item -Path "$(Get-Location)\README.md" -Destination "$ProjectPath\README.md" -Force
     Copy-Item -Path "$ProjectPath\README.md" -Destination "$PackagePath\README.md" -Force
     Compress-Archive -Path "$PackagePath\*" -DestinationPath "$TargetPath\$TargetAssembly.zip" -Force
 }
